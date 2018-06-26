f6163dcf5140d312a73c61554800756c40009989
Luis Farfán Melgar <lfarfan.skylogistics@gmail.com>
2018-06-26 10:51:26 -0500

    getIn terminado, realizado los test

--- index.js
+++ index.js
@@ -15,21 +15,37 @@
  * Happy Conding,
  * */
 
-var object = {'a': {'b': {'c': 3}}};
+var obj = {'a': {'b': {'c': 3}}};
 
 const getIn = (object, path, defaultValue) => {
     if (typeof object !== 'object' || typeof path !== 'string') {
         return defaultValue;
     }
+
+    const get = (obj, k, d) => obj[k] ? obj[k] : d;
+
     const pathSplited = path.split('.');
     const keyToFind = pathSplited.length ? pathSplited[0] : path;
+
     if (pathSplited.length === 0 || pathSplited.length === 1) {
-        return object[keyToFind] ? object[keyToFind] : defaultValue;
+        return get(object, keyToFind, defaultValue);
     } else {
+        let value = object;
         for (let i = 0; i <= pathSplited.length; i++) {
             if (i === pathSplited.length - 1) {
-
+                if (typeof value === 'object' && pathSplited[i] in value) return value[pathSplited[i]];
+                else return defaultValue;
+            } else {
+                if (pathSplited[i] in value) {
+                    value = value[pathSplited[i]];
+                } else {
+                    return defaultValue;
+                }
             }
         }
     }
 }
+
+/** TEST! */
+getIn(obj, 'a.b.c', 10)
+getIn(obj, 'a.b.c.d', 10)
